8c2b80b84701f7b456e5fd9d99dc6a81db861ee1
Dima_552 <foker552@gmail.com>
2021-03-29 14:26:57 +0300

    bug fixes 2

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,9 @@
 
 import { NewGameResonse } from './types/new-game-resp';
-import { Games, Game } from './types/games';
+import { Game } from './types/games';
 import { Controller, Get, Post, Param, GatewayTimeoutException, Query} from '@nestjs/common';
 import { GameService } from './app.service';
 
-type GameDTO = {
-  id?: string,
-}
 
 @Controller()
 export class AppController {
@@ -19,8 +16,9 @@ export class AppController {
 
 
   @Get('/games/:id')
-  game(@Param('id') id: GameDTO): Game {
-    return this.gameService.game(id);
+  getGame(@Param('id') id: string): Game {
+
+    return this.gameService.getGame(id);
   }
 
   @Post('/game')
